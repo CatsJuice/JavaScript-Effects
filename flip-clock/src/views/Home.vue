b0e6ea3e01a2c46c0a55ccaf010acc664a5c67d5
CatsJuice <wp1298554944@hotmail.com>
2020-01-10 21:20:24 +0800

    temp

---
+++ flip-clock/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <flip-clock :options="options" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { FlipClock } from "@mvpleung/flipclock";
+
+export default {
+  name: "home",
+  components: {
+    FlipClock
+  },
+  data() {
+    return {
+      options: {
+
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.home {
+  .flip-clock-wrapper ul li a div div.inn {
+    background-color: #333;
+  }
+
+  .flip-clock-divider .flip-clock-label {
+    color: #f00 !important;
+  }
+}
+</style>
